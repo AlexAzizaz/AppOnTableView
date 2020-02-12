9ddbc3aae8d070fddb5212898d064e81f0b2ffac
Alex <88_n1@mail.ru>
2020-02-12 23:25:48 +0300

    custom table view cell

---
+++ AppOnTableView/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  AppOnTableView
+//
+//  Created by onyx on 12.02.2020.
+//  Copyright © 2020 Alex Al. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLable: UILabel!
+    @IBOutlet weak var locationLable: UILabel!
+    @IBOutlet weak var typeLable: UILabel!
+
+    
+
+}
